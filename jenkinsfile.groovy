d7314be31523f0a2bc4286be1da7837b2c9f9953
niluminda <kvcniluminda@gmail.com>
2025-11-11 00:00:15 +0530

    docker build path is corrected

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -17,7 +17,7 @@ pipeline {
         stage('Build Docker Image') {
             steps {
                 script {
-                    sh "docker build -t ${IMAGE_NAME}:latest -f frontend/userlogin/Dockerfile ."
+                    sh "docker build -t ${IMAGE_NAME}:latest -f frontend/userlogin/Dockerfile frontend/userlogin"
                 }
             }
         }
